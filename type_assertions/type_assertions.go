2ad2a7b0086175ae981e06c5a2f1864a4948e9ac
Endryco Farel Rianrachmatullah <88662707+endrycofr@users.noreply.github.com>
2025-12-15 21:03:46 +0700

    menambahkan penjelasan type assertions

--- type_assertions/type_assertions.go
+++ type_assertions/type_assertions.go
@@ -1,18 +1,18 @@
 package main
 
-func checktype(name interface{}) {
-	switch value := name.(type) {
-	case string:
+func checktype(name interface{}) { //fungsi untuk memeriksa tipe data dari parameter name yang bertipe interface{}
+	switch value := name.(type) { //melakukan type assertion menggunakan switch
+	case string: //jika tipe data adalah string
 		println("Name is a string:", value)
-	case int:
+	case int: //jika tipe data adalah int
 		println("Name is an integer:", value)
-	default:
+	default: //jika tipe data tidak dikenali
 		println("Unknown type")
 	}
 }
 
 func main() {
-	checktype("Alice")
-	checktype(42)
-	checktype(true)
+	checktype("Alice") //memanggil fungsi checktype dengan parameter string
+	checktype(42)      //memanggil fungsi checktype dengan parameter int
+	checktype(true)    //memanggil fungsi checktype dengan parameter boolean
 }
